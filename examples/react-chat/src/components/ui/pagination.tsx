b8dd9ed97879ec0688ea92fa46003391ac4113c5
Francesco Cartier <francescocartier@gmail.com>
2026-04-22 14:30:57 +0200

    lint

--- examples/react-chat/src/components/ui/pagination.tsx
+++ examples/react-chat/src/components/ui/pagination.tsx
@@ -107,9 +107,9 @@ PaginationEllipsis.displayName = "PaginationEllipsis";
 export {
   Pagination,
   PaginationContent,
-  PaginationLink,
-  PaginationItem,
-  PaginationPrevious,
-  PaginationNext,
   PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
 };
